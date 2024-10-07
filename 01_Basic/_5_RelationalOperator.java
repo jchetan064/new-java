e9dba797dc9f2539fcffd3fede61b4e3c3b0b7e1
jchetan064 <113768496+jchetan064@users.noreply.github.com>
2024-10-07 09:13:25 +0000

    Relational Operators

---
+++ 01_Basic/_5_RelationalOperator.java
@@ -0,0 +1,21 @@
+/**
+ * _5_RelationalOperator
+ */
+public class _5_RelationalOperator {
+
+    public static void main(String[] args) {
+        
+
+        // These Operators are gives the o/p in true or false format 
+
+        int a = 10;
+        int b = 12;
+
+        System.out.println( a > b );
+        System.out.println( a < b );
+        System.out.println( a == b );
+        System.out.println( a >= b );
+        System.out.println( a <= b );
+        System.out.println( a != b );
+    }
+}
