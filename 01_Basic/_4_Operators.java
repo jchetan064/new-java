16dfdbb68cbf319605cd10ad563f286cc958ccac
jchetan064 <113768496+jchetan064@users.noreply.github.com>
2024-10-07 09:06:50 +0000

    Assignment Operators

---
+++ 01_Basic/_4_Operators.java
@@ -0,0 +1,27 @@
+
+public class _4_Operators {
+
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 23;
+
+        System.out.println( a + b );
+        System.out.println( a - b );
+        System.out.println( a * b );
+        System.out.println( a / b );
+        System.out.println( a % b );
+
+
+        // a++ -> Post Increment
+        // --a -> Pre Increment
+
+        System.out.println( a++ );
+        System.out.println( a-- );
+        System.out.println( a += 20 );
+        System.out.println( a -= 2 );
+        System.out.println( a *= 2 );
+        System.out.println( a /= 12 );
+
+    }
+}
