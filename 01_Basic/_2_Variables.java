223c15ab40896c07c03a68a147efc1a323e38c0a
jchetan064 <113768496+jchetan064@users.noreply.github.com>
2024-10-07 08:39:14 +0000

    Details About Variables

---
+++ 01_Basic/_2_Variables.java
@@ -0,0 +1,35 @@
+/**
+ * _1_first
+ */
+class _2_Variables {
+
+    public static void main(String[] args) {
+
+        // integer  -> byte(1) , short(2) , int(4) , long(8)
+       
+            
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        // System.out.println( c );
+
+
+
+        // double  -> double (8), float(4) 
+        
+        double a1 = 8.9403934;
+        float b1 = 898.44353f;
+
+        System.out.println(a1);
+        System.out.println(b1);
+        
+        // Char  -> char , string
+
+        char c1 = 'q';
+        System.out.println(c1); 
+        
+        // boolean 
+        boolean d = true;
+        System.out.println(d);
+    }
+}
