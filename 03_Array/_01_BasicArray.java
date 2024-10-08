b772e49820ceebe04b1dc6072f4a389d85400cf8
jchetan064 <113768496+jchetan064@users.noreply.github.com>
2024-10-08 08:27:10 +0000

    Basic Creation of array

---
+++ 03_Array/_01_BasicArray.java
@@ -0,0 +1,16 @@
+public class _01_BasicArray {
+    public static void main(String[] args) {
+
+        int nums[] = {1,2,3,4,5};
+        
+        // System.out.println(nums[0]);
+        // System.out.println(nums[1]);
+        // System.out.println(nums[2]);
+        // System.out.println(nums[3]);
+        // System.out.println(nums[4]);
+
+        for( int i = 0; i < nums.length; i++ ){
+            System.out.println(nums[i]);
+        }
+    }
+}
