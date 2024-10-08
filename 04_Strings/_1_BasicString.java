c93b9d1df38229bf65f098e89921a4a178b527de
jchetan064 <113768496+jchetan064@users.noreply.github.com>
2024-10-08 10:14:07 +0000

    Basic String

---
+++ 04_Strings/_1_BasicString.java
@@ -0,0 +1,11 @@
+public class _1_BasicString {
+	public static void main(String[] args) 
+	{
+		String name = new String("Chetan");
+		System.out.println(name);
+		System.out.println(name.hashCode());
+		System.out.println("hello "+name);
+		System.out.println(name.concat("Joshi"));
+		
+	}
+}
