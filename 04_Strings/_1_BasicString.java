a491b4ed6acd0de87b3913e0992ea353d4ee966d
jchetan064 <113768496+jchetan064@users.noreply.github.com>
2024-10-08 10:23:49 +0000

    update

--- 04_Strings/_1_BasicString.java
+++ 04_Strings/_1_BasicString.java
@@ -1,11 +1,13 @@
 public class _1_BasicString {
 	public static void main(String[] args) 
 	{
-		String name = new String("Chetan");
+		// String name = new String("Chetan");
+		String name = "Chetan";
 		System.out.println(name);
+		name = "Chelsye";
 		System.out.println(name.hashCode());
 		System.out.println("hello "+name);
-		System.out.println(name.concat("Joshi"));
+		System.out.println(name.concat(" Joshi"));
 		
 	}
 }
