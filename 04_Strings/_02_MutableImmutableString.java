ed7d44da32823e4b0984f8c8f428b323467451d4
jchetan064 <113768496+jchetan064@users.noreply.github.com>
2024-10-08 10:35:49 +0000

    update

---
+++ 04_Strings/_02_MutableImmutableString.java
@@ -0,0 +1,17 @@
+
+
+public class _02_MutableImmutableString {
+        public static void main(String[] args) 
+        {
+            String name="chetan";
+            name=name+"Joshi";
+            System.out.println("hello"+name);
+            
+            String s1="Chetan";
+            String s2="Chetan";
+            
+            System.out.println(s1==s2);	
+            
+        }
+    }
+
